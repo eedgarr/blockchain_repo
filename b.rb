063d1b728915072e273ebf034b6c1a4eb8368896
eedgarr <bord0913@naver.com>
2018-04-23 22:02:07 +0900

    first memo
    첫번째 소스

---
+++ b.rb
@@ -0,0 +1,12 @@
+ require 'sinatra'
+ 
+
+
+get '/blockchain' do
+   '<h1>you mine a block<h1>'
+end
+
+get 'edgar' do
+   '<h1> Edgar is a professional trader and a blockchain enthusiast.<h1>'
+end
+
